379d09685f1569666f6070e9562efdc39ff4d8d5
Drishti Saraf <drishtisaraf0307@gmail.com>
2025-05-09 12:31:14 +0530

    Add gh-pages dependency and update deploy script in package.json

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import ghPages from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
+  base: "/portfolio-threejs/", // Use your repo name here
   server: {
     allowedHosts: true,
   },
-  base:"https://github.com/drishti1920/portfolio-threejs"
 });
